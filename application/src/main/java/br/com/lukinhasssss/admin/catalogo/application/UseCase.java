b82fe3361fce882387c1951caf58ef7927326347
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-07 17:54:19 -0300

    refactor: refact exceptions

--- application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/UseCase.java
+++ application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/UseCase.java
@@ -1,7 +1,5 @@
 package br.com.lukinhasssss.admin.catalogo.application;
 
-import br.com.lukinhasssss.admin.catalogo.domain.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
