aadaadc55fffc7ddf05756d551b051848d6e8c85
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-07 15:28:07 -0300

    test: add tests for entity category

---
+++ domain/src/test/java/br/com/lukinhasssss/admin/catalogo/domain/category/CategoryTest.java
@@ -0,0 +1,28 @@
+package br.com.lukinhasssss.admin.catalogo.domain.category;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory =
+            Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertNotNull(actualCategory);
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+    }
+
+}
