e1cce0a005f72fadc13d4bc89668a46dae1b18dd
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-10 20:14:55 -0300

    feat: add notification pattern as a validation handler and work with either in create category usecase

--- application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -1,6 +1,9 @@
 package br.com.lukinhasssss.admin.catalogo.application.category.create;
 
 import br.com.lukinhasssss.admin.catalogo.application.UseCase;
+import br.com.lukinhasssss.admin.catalogo.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase
+    extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 }
