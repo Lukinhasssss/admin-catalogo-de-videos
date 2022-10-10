da21c37e3432802b6156025ffcb1f9d0113e9c03
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-10 18:13:58 -0300

    test: add more tests for implementation of create category usecase

--- application/src/test/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,15 +1,27 @@
 package br.com.lukinhasssss.admin.catalogo.application.category.create;
 
 import br.com.lukinhasssss.admin.catalogo.domain.category.CategoryGateway;
-import org.junit.jupiter.api.Assertions;
+import br.com.lukinhasssss.admin.catalogo.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CreateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
 
     @Test
     void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
@@ -17,29 +29,96 @@ class CreateCategoryUseCaseTest {
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
-        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var aCommand =
+            CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
 
-        final CategoryGateway categoryGateway = mock(CategoryGateway.class);
-
-        when(categoryGateway.create(any()))
-            .thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
+        when(categoryGateway.create(any())).thenAnswer(returnsFirstArg());
 
         final var actualOutput = useCase.execute(aCommand);
 
-        Assertions.assertNotNull(actualOutput);
-        Assertions.assertNotNull(actualOutput.id());
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
 
         verify(categoryGateway, times(1))
-            .create(argThat(aCategory -> {
-                return Objects.equals(expectedName, aCategory.getName())
-                    && Objects.equals(expectedDescription, aCategory.getDescription())
-                    && Objects.equals(expectedIsActive, aCategory.isActive())
-                    && Objects.nonNull(aCategory.getId())
-                    && Objects.nonNull(aCategory.getCreatedAt())
-                    && Objects.nonNull(aCategory.getUpdatedAt())
-                    && Objects.isNull(aCategory.getDeletedAt());
-            }));
+            .create(argThat(aCategory -> Objects.equals(expectedName, aCategory.getName())
+                && Objects.equals(expectedDescription, aCategory.getDescription())
+                && Objects.equals(expectedIsActive, aCategory.isActive())
+                && Objects.nonNull(aCategory.getId())
+                && Objects.nonNull(aCategory.getCreatedAt())
+                && Objects.nonNull(aCategory.getUpdatedAt())
+                && Objects.isNull(aCategory.getDeletedAt())));
+    }
+
+    @Test
+    void givenAnInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var aCommand =
+            CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+            assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        verify(categoryGateway, times(0)).create(any());
+    }
+
+    @Test
+    void givenAValidCommandWithInactiveCategory_whenCallsCreateCategory_shouldReturnInactiveCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var aCommand =
+            CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any())).thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        verify(categoryGateway, times(1))
+            .create(argThat(aCategory -> Objects.equals(expectedName, aCategory.getName())
+                && Objects.equals(expectedDescription, aCategory.getDescription())
+                && Objects.equals(expectedIsActive, aCategory.isActive())
+                && Objects.nonNull(aCategory.getId())
+                && Objects.nonNull(aCategory.getCreatedAt())
+                && Objects.nonNull(aCategory.getUpdatedAt())
+                && Objects.nonNull(aCategory.getDeletedAt())));
+    }
+
+    @Test
+    void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnAnException() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aCommand =
+            CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        when(categoryGateway.create(any()))
+            .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException =
+            assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        verify(categoryGateway, times(1))
+            .create(argThat(aCategory -> Objects.equals(expectedName, aCategory.getName())
+                && Objects.equals(expectedDescription, aCategory.getDescription())
+                && Objects.equals(expectedIsActive, aCategory.isActive())
+                && Objects.nonNull(aCategory.getId())
+                && Objects.nonNull(aCategory.getCreatedAt())
+                && Objects.nonNull(aCategory.getUpdatedAt())
+                && Objects.isNull(aCategory.getDeletedAt())));
     }
 }
