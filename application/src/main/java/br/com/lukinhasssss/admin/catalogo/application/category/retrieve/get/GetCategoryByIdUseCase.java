9bc981c63a9f66feade03e11996f7e663116456f
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-11 14:58:10 -0300

    feat: add implementation of find category by id usecase

---
+++ application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package br.com.lukinhasssss.admin.catalogo.application.category.retrieve.get;
+
+import br.com.lukinhasssss.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase
+    extends UseCase<String, CategoryOutput> { }
