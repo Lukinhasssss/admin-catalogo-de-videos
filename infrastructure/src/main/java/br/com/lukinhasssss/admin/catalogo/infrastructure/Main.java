781781b888c15376378fabb4c1a1b5a5bfbb0141
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-12 11:43:09 -0300

    feat: create implementation of category gateway

--- infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/Main.java
@@ -1,13 +1,8 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure;
 
-import br.com.lukinhasssss.admin.catalogo.domain.category.Category;
-import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
-import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.configuration.WebServerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -15,18 +10,5 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @Bean
-    public ApplicationRunner runner(CategoryRepository repository) {
-        return args -> {
-            var all = repository.findAll();
-
-            var filmes = Category.newCategory("Filmes", null, true);
-
-            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
-
-            repository.deleteAll();
-        };
     }
 }
