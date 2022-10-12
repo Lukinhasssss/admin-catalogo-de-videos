e362a792a68c1d935187041f66d21541448f06b1
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-12 15:14:25 -0300

    test: add test for implementation of delete by id method of category gateway

--- infrastructure/src/test/java/br/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/br/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,6 +1,7 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure.category;
 
 import br.com.lukinhasssss.admin.catalogo.domain.category.Category;
+import br.com.lukinhasssss.admin.catalogo.domain.category.CategoryID;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.MySQLGatewayTest;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
@@ -71,13 +72,13 @@ class CategoryMySQLGatewayTest {
 
         categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
 
+        assertEquals(1, categoryRepository.count());
+
         final var actualInvalidEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
 
         assertEquals("Filme", actualInvalidEntity.getName());
         assertNull(actualInvalidEntity.getDescription());
         assertEquals(expectedIsActive, actualInvalidEntity.isActive());
-
-        assertEquals(1, categoryRepository.count());
 
         final var anUpdatedCategory =
             aCategory.clone().update(expectedName, expectedDescription, expectedIsActive);
@@ -104,5 +105,29 @@ class CategoryMySQLGatewayTest {
         assertTrue(aCategory.getUpdatedAt().isBefore(actualEntity.getUpdatedAt()));
         assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
         assertNull(actualEntity.getDeletedAt());
+    }
+
+    @Test
+    void givenAPrePersistedCategoryAndValidCategoryId_whenTryToDeleteIt_shouldDeleteCategory() {
+        final var aCategory = Category.newCategory("Filmes", null, true);
+
+        assertEquals(0, categoryRepository.count());
+
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+
+        assertEquals(1, categoryRepository.count());
+
+        categoryGateway.deleteById(aCategory.getId());
+
+        assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    void givenAnInvalidCategoryId_whenTryToDeleteIt_shouldDeleteCategory() {
+        assertEquals(0, categoryRepository.count());
+
+        categoryGateway.deleteById(CategoryID.from("invalid"));
+
+        assertEquals(0, categoryRepository.count());
     }
 }
