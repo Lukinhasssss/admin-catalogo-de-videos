fd5dbf86092d27b96542f59e4d7b16bebc04d580
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-11 23:04:23 -0300

    build: configure application test datasource

--- infrastructure/src/test/java/br/com/lukinhasssss/admin/catalogo/infrastructure/MainTest.java
+++ infrastructure/src/test/java/br/com/lukinhasssss/admin/catalogo/infrastructure/MainTest.java
@@ -1,11 +1,15 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
+
 class MainTest {
 
-//    @Test
-//    public void testMain() {
-//        Assertions.assertNotNull(new Main());
-//        Main.main(new String[]{});
-//    }
-
+    @Test
+    void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
+        Assertions.assertNotNull(new Main());
+        Main.main(new String[]{});
+    }
 }
