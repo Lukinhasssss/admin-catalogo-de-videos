100c67d96d74dd9894cee1a14f419958a835cfca
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-13 10:08:58 -0300

    feat: implement find all method of category gateway

--- infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -7,6 +7,11 @@ import br.com.lukinhasssss.admin.catalogo.domain.category.CategorySearchQuery;
 import br.com.lukinhasssss.admin.catalogo.domain.pagination.Pagination;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import br.com.lukinhasssss.admin.catalogo.infrastructure.utils.SpecificationUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -44,9 +49,35 @@ public class CategoryMySQLGateway implements CategoryGateway {
         return save(aCategory);
     }
 
+    /**
+     * Paginação
+     * Busca dinãmica pelo critério terms (name ou description)
+     */
     @Override
     public Pagination<Category> findAll(CategorySearchQuery aQuery) {
-        return null;
+        final var page = PageRequest.of(
+            aQuery.page(),
+            aQuery.perPage(),
+            Sort.by(Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var specifications = Optional.ofNullable(aQuery.terms())
+            .filter(str -> !str.isBlank())
+            .map(str -> {
+                final Specification<CategoryJpaEntity> nameLike = SpecificationUtils.like("name", str);
+                final Specification<CategoryJpaEntity> descriptionLike = SpecificationUtils.like("description", str);
+                return nameLike.or(descriptionLike);
+            })
+            .orElse(null);
+
+        final var pageResult = categoryRepository.findAll(Specification.where(specifications), page);
+
+        return new Pagination<>(
+            pageResult.getNumber(),
+            pageResult.getSize(),
+            pageResult.getTotalElements(),
+            pageResult.map(CategoryJpaEntity::toAggregate).toList()
+        );
     }
 
     private Category save(final Category aCategory) {
