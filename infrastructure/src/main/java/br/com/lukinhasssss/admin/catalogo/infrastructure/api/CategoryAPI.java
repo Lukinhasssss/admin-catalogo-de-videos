dae560b0eed8cc05280a4c8df7825740a3cfe66a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-15 20:50:42 -0300

    docs: start to add springdocs openapi annotations

--- infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/CategoryAPI.java
@@ -1,6 +1,10 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure.api;
 
 import br.com.lukinhasssss.admin.catalogo.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +14,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping(value = "categories")
+@Tag(name = "Categories")
 public interface CategoryAPI {
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Created successfully"),
+        @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Listed successfully"),
+        @ApiResponse(responseCode = "422", description = "An invalid parameter was received"),
+        @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    })
     Pagination<?> listCategories(
         @RequestParam(name = "search", required = false, defaultValue = "") final String search,
         @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
