9a8ee75343b8dbd803ad6b1215f18ccef0161411
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-15 22:26:07 -0300

    feat: add implementation of create category endpoint

--- application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/br/com/lukinhasssss/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -1,13 +1,16 @@
 package br.com.lukinhasssss.admin.catalogo.application.category.create;
 
 import br.com.lukinhasssss.admin.catalogo.domain.category.Category;
-import br.com.lukinhasssss.admin.catalogo.domain.category.CategoryID;
 
 public record CreateCategoryOutput(
-    CategoryID id
+    String id
 ) {
 
+    public static CreateCategoryOutput from(final String anId) {
+        return new CreateCategoryOutput(anId);
+    }
+
     public static CreateCategoryOutput from(final Category aCategory) {
-        return new CreateCategoryOutput(aCategory.getId());
+        return new CreateCategoryOutput(aCategory.getId().getValue());
     }
 }
--- infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,16 +1,43 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure.api.controllers;
 
+import br.com.lukinhasssss.admin.catalogo.application.category.create.CreateCategoryCommand;
+import br.com.lukinhasssss.admin.catalogo.application.category.create.CreateCategoryOutput;
+import br.com.lukinhasssss.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import br.com.lukinhasssss.admin.catalogo.domain.pagination.Pagination;
+import br.com.lukinhasssss.admin.catalogo.domain.validation.handler.Notification;
 import br.com.lukinhasssss.admin.catalogo.infrastructure.api.CategoryAPI;
+import br.com.lukinhasssss.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.Objects;
+import java.util.function.Function;
 
 @RestController
 public class CategoryController implements CategoryAPI {
 
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
+
     @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
+    public ResponseEntity<?> createCategory(final CreateCategoryApiInput input) {
+        final var aCommand = CreateCategoryCommand.with(
+            input.name(),
+            input.description(),
+            input.active() != null ? input.active() : true
+        );
+
+        final Function<Notification, ResponseEntity<?>> onError = notification ->
+            ResponseEntity.unprocessableEntity().body(notification);
+
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
+            ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
+
+        return createCategoryUseCase.execute(aCommand).fold(onError, onSuccess);
     }
 
     @Override
