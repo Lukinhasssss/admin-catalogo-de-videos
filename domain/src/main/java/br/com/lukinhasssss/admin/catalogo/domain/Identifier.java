89f9bc7d98be51c0c8fb840cd91b230130ea08ea
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-26 23:20:44 -0300

    feat: add error handler for not found exception

--- domain/src/main/java/br/com/lukinhasssss/admin/catalogo/domain/Identifier.java
+++ domain/src/main/java/br/com/lukinhasssss/admin/catalogo/domain/Identifier.java
@@ -1,4 +1,6 @@
 package br.com.lukinhasssss.admin.catalogo.domain;
 
 public abstract class Identifier extends ValueObject {
+
+    public abstract String getValue();
 }
--- infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/br/com/lukinhasssss/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.lukinhasssss.admin.catalogo.infrastructure.api.controllers;
 
 import br.com.lukinhasssss.admin.catalogo.domain.exceptions.DomainException;
+import br.com.lukinhasssss.admin.catalogo.domain.exceptions.NotFoundException;
 import br.com.lukinhasssss.admin.catalogo.domain.validation.Error;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<?> handlerNotFoundException(final NotFoundException ex) {
+        return ResponseEntity.status(NOT_FOUND).body(ApiError.from(ex));
+    }
 
     @ExceptionHandler(value = DomainException.class)
     public ResponseEntity<?> handlerDomainException(final DomainException ex) {
