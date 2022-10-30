9be7c091f341fe9b222284e19daea6a98207b5b6
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-10-30 19:11:42 -0300

    feat: create strategy for e2e tests with testcontainers

---
+++ infrastructure/src/test/java/br/com/lukinhasssss/admin/catalogo/E2ETest.java
@@ -0,0 +1,17 @@
+package br.com.lukinhasssss.admin.catalogo;
+
+import br.com.lukinhasssss.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-e2e")
+@SpringBootTest(classes = WebServerConfig.class)
+@ExtendWith(MySQLCleanUpExtension.class)
+public @interface E2ETest {
+}
